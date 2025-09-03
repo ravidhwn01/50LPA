6061b86d6857771fb8a4cb08c6a627b514fc303a
ravi <ravidhawan8899@gmail.com>
2025-09-03 21:53:31 +0530

    print Binary Number Triangle pattern

---
+++ patterns/binary_number_triangle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void printBinaryNumberTriangle(int n)
+{
+    int start = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+            start = 1;
+        else
+            start = 0;
+
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << start << " ";
+            start = 1 - start;
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n = 5;
+    printBinaryNumberTriangle(n);
+    return 0;
+}
