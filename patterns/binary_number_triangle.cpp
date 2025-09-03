1cd69a6abd9b850dfc96b1a31c05b3434151250e
ravi <ravidhawan8899@gmail.com>
2025-09-03 22:37:58 +0530

    print Increasing Number Triangle

--- patterns/binary_number_triangle.cpp
+++ patterns/binary_number_triangle.cpp
@@ -20,9 +20,30 @@ void printBinaryNumberTriangle(int n)
         cout << endl;
     }
 }
+
+void printNumberCrown(int n)
+{
+}
+
+void printIncreasingNumberTriangle(int n)
+{
+    int count = 1;   // i forgot to initialize count to 1
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << count << " ";
+            count++;
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int n = 5;
     printBinaryNumberTriangle(n);
+    printNumberCrown(n);
+    printIncreasingNumberTriangle(n);
     return 0;
-}
+    }
