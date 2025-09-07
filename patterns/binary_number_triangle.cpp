655b89b3959dccd1af402ae2ac78880ab4eea820
ravi <ravidhawan8899@gmail.com>
2025-09-07 23:17:49 +0530

    print Hollow Rectangle

--- patterns/binary_number_triangle.cpp
+++ patterns/binary_number_triangle.cpp
@@ -245,6 +245,23 @@ void  printSymmetricButterflyPattern(int n){
         
 }
 
+void printHollowRectangle(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if(i==0 || i==n-1 || j==0 || j==n-1){
+                cout<<" * ";
+            }
+            else{
+                cout<<"   ";
+            }
+        }
+        cout<<endl;
+    }
+    
+}
+
 int main()
 {
     int n = 5;
@@ -264,5 +281,7 @@ int main()
     // printSymmetricVoidPattern(n);
     cout<<endl;
     printSymmetricButterflyPattern(n);
+    cout<<endl;
+    printHollowRectangle(n);
     return 0;
 }
